1005895567d59dfd1ed41822092d6520f748ef83
AMHOL <andyholland1991@aol.com>
2018-06-29 00:04:55 +0800

    Use uint32 for frame number

--- lib/openra/replays/order_list.rb
+++ lib/openra/replays/order_list.rb
@@ -2,7 +2,7 @@ module Openra
   module Replays
     class OrderList < BinData::Record
       endian :little
-      int32 :frame
+      uint32 :frame
       array :orders, type: :order, read_until: :eof
     end
   end
