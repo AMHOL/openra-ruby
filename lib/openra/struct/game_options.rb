c158776ed2b0efd9235b229a03117f8217253ab3
AMHOL <andyholland1991@aol.com>
2018-07-03 21:59:06 +0800

    Make new game options optional

--- lib/openra/struct/game_options.rb
+++ lib/openra/struct/game_options.rb
@@ -6,15 +6,36 @@ module Openra
         attribute :cheats_enabled, BooleanOption.meta(from: 'cheats')
         attribute :explored_map_enabled, BooleanOption.meta(from: 'explored')
         attribute :fog_enabled, BooleanOption.meta(from: 'fog')
-        attribute :bounties_enabled, BooleanOption.meta(from: 'bounty')
-        attribute :conyard_undeploy_enabled, BooleanOption.meta(from: 'factundeploy')
+        attribute :bounties_enabled, BooleanOption.meta(from: 'bounty', omittable: true)
+        attribute :conyard_undeploy_enabled, BooleanOption.meta(from: 'factundeploy', omittable: true)
         attribute :crates_enabled, BooleanOption.meta(from: 'crates')
         attribute :build_off_allies_enabled, BooleanOption.meta(from: 'allybuild')
-        attribute :restricted_build_radius_enabled, BooleanOption.meta(from: 'buildradius')
+        attribute :restricted_build_radius_enabled, BooleanOption.meta(from: 'buildradius', omittable: true)
         attribute :short_game_enabled, BooleanOption.meta(from: 'shortgame')
         attribute :tech_level, StringOption.meta(from: 'techlevel')
         attribute :game_speed, StringOption.meta(from: 'gamespeed')
         attribute :starting_units, StringOption.meta(from: 'startingunits')
+      end
+
+      def bounties_enabled
+        attributes.fetch(:bounties_enabled, BooleanOption.new(
+          value: true,
+          preferred_value: true
+        ))
+      end
+
+      def conyard_undeploy_enabled
+        attributes.fetch(:conyard_undeploy_enabled, BooleanOption.new(
+          value: true,
+          preferred_value: true
+        ))
+      end
+
+      def restricted_build_radius_enabled
+        attributes.fetch(:restricted_build_radius_enabled, BooleanOption.new(
+          value: true,
+          preferred_value: true
+        ))
       end
     end
   end
